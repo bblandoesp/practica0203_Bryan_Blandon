a750f2b3f492b9347df658cc40d23471bcc429cb
bblandoesp <bblandoesp@educacion.navarra.es>
2023-10-30 19:48:41 +0100

    ¡Yo estudio!

---
+++ Ejercicio 2.py
@@ -0,0 +1,3 @@
+list1 = ["matemáticas", "Física", "Química", "Historia", "Lengua"]
+x = ("yo estudio:")
+print (x, list1[0],"\n", x, list1[1],"\n", x, list1[2],"\n", x, list1[3],"\n", x, list1[4], "\n" )
