b64fa02d0112eaf287c60f2a435fef41768a1719
bblandoesp <bblandoesp@educacion.navarra.es>
2023-11-02 23:32:13 +0100

    Nota en las clases

---
+++ Ejercicio 3.py
@@ -0,0 +1,8 @@
+list1 = ["Matemáticas", "Física", "Química", "Historia", "Lengua"]
+n_m = float (input ("Nota de Matemáticas:"))
+n_f = float (input ("Nota de Física:"))
+n_q = float (input ("Nota de Química:"))
+n_h = float (input ("Nota de Historia:"))
+n_l = float (input ("Nota de Lengua:"))
+print ("En", list1[0], "has sacado:", n_m, "\n", "En", list1[1], "has sacado:", n_f, "\n", "En", list1[2], "has sacado:", n_q, "\n",
+       "En", list1[3], "has sacado:", n_h, "\n", "En", list1[4], "has sacado:", n_l)
